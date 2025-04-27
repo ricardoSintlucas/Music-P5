645c08c3a549e2a9580a8b8e2d4386dc9ff92a71
Ricardo Creemers <127936801+ricardoSintlucas@users.noreply.github.com>
2025-04-27 13:10:27 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,13 +2,36 @@ let song;
 let fft;
 let w;
 let button;
+let spectrum;
+let bgColor = "#000000";
+let prevRangeValue;
+let newRangeValue;
+//bass values
+let prevBassValue;
+let newBassValue;
+//high head values
+let prevHighHeadValue;
+let newHighHeadValue;
+//noise values
+let xNoise = 0;
+let yNoise = 0;
+let addValue = 0;
 
+let offsetValue = 30;
 function preload() {
   song = loadSound("Da Funk.mp3");
 }
 
 function setup() {
   createCanvas(640, 640);
+  ellipseMode(CENTER)
+  createP("Spectrum Analyzer");
+  sliderSpectrum = createSlider(0,64 );
+  createP("Spectrum peak value");
+  sliderPeakValue = createSlider(0,255 );
+
+  addValue = random(0, 100);
+  
   colorMode(HSB);
   angleMode(DEGREES);
   button = createButton("toggle");
@@ -16,7 +39,7 @@ function setup() {
   song.play();
 
   // Set smoothing to 0, 256 bins
-  fft = new p5.FFT(0.8, 64);
+  fft = new p5.FFT(0.6, 64);
   w = width / 64;
 }
 
@@ -29,39 +52,38 @@ function toggleSong() {
 }
 
 function draw() {
-  background(0);
-  let spectrum = fft.analyze();
-  //console.log(spectrum[17]);
-  if (spectrum[17] > 200) {
-    console.log("hi");
-    ellipse;
-  }
-  if (spectrum[45] > 50) {
-    console.log("yo");
-    ellipse(200, 100, 50, 50);
-  }
-  // stroke(255);
-  // for (let i = 0; i < spectrum.length; i++) {
-  //   let ampl = spectrum[i];
+  background(bgColor);
+  offsetValue= round(map(sliderPeakValue.value(), 0, 255, 0, 80));
+  spectrum = fft.analyze();
+  let spectrumHerz = spectrum[sliderSpectrum.value()];
+  let circleSize = map(spectrumHerz, 0, 255, 0, 100);
 
-  //   let y = map(ampl, 0, 256, height, 0);
-  //   rect(i * w, y, w, height - y);
+  oldRangeValue = newRangeValue;
+  newRangeValue = spectrumHerz;
+  // if(abs(oldRangeValue-newRangeValue) > offsetValue) {
+  //   //
   // }
-  // Default length is 1024;
-  // console.log(spectrum.length);
+  //bass check
+  prevBassValue = newBassValue;
+  newBassValue = spectrum[2];
+  if(abs(prevBassValue-newBassValue) > 50) {
+    bgColor = color(random(100), random(100), random(100));
+  }
 
-  // translate(width / 2, height / 2);
-  // //beginShape();
-  // for (let i = 0; i < spectrum.length; i++) {
-  //   let angle = map(i, 0, spectrum.length, 0, 360);
-  //   let ampl = spectrum[i];
-  //   let r = map(ampl, 0, 256, 20, 100);
-  //   //fill(i, 255, 255);
-  //   let x = r * cos(angle);
-  //   let y = r * sin(angle);
-  //   stroke(i, 255, 255);
-  //   line(0, 0, x, y);
-  //   //vertex(x, y);
-  // }
-  // //endShape();
+  //high head check
+  prevHighHeadValue = newHighHeadValue;
+  newHighHeadValue = spectrum[33];
+  if(abs(prevHighHeadValue-newHighHeadValue) > 50) {
+    addValue +=100;
+  }
+  xNoise = noise(0.001 * addValue);
+  yNoise = noise(0.001 * addValue);
+  push();
+  fill(255);
+  text("Spectrum Value "+sliderSpectrum.value(), 10, 10);
+  text("Peak Value "+offsetValue, 10, 30);
+  text(abs(oldRangeValue-newRangeValue), 10, 50);
+  pop();
+  
+  ellipse(width/2*xNoise, height/2*yNoise, circleSize, circleSize);
 }
